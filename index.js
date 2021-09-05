d9158a1fd21db27695793547cb014186cbb3362c
johnshaw77 <john.hsiaocc@gmail.com>
2021-09-05 17:57:28 +0800

    add express

---
+++ index.js
@@ -0,0 +1,15 @@
+// require 與 import 的差界
+// https://blog.niclin.tw/2019/10/03/nodejs-require-vs-es6-import-export/
+// Node.js SyntaxError：Cannot use import statement outside a module
+// https://blog.twtnn.com/2021/05/nodejs-syntaxerrorcannot-use-import.html
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('<h1 style="color:red;font-size:164px;margin:400px 50px;">API Server for FUN</h1>'))
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log('目前的port是', port);
+});
+
+
